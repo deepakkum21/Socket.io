f29f1b89378748d3d880c48a9de2eca8d35ccfcd
deepakkum21 <43340283+deepakkum21@users.noreply.github.com>
2020-07-31 16:12:10 +0530

    Socket-demo/ socket.io rooms'

--- Sockect Concept Demo/app.js
+++ Sockect Concept Demo/app.js
@@ -77,8 +77,10 @@ app.get('/', function (req, res) {
 var roomno = 1;
 io.on('connection', function (socket) {
 
-    //Increase roomno 2 clients are present in a room.
-    if (io.nsps['/'].adapter.rooms["room-" + roomno] && io.nsps['/'].adapter.rooms["room-" + roomno].length > 1) roomno++;
+    //Increase roomno if 2 clients are present in a room.
+    if (io.nsps['/'].adapter.rooms["room-" + roomno] && io.nsps['/'].adapter.rooms["room-" + roomno].length > 1) {
+        roomno++;
+    }
     socket.join("room-" + roomno);
 
     //Send this event to everyone in the room.
