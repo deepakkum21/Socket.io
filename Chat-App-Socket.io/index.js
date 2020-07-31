df0f2844e230e89fd908ec0e01fe02e1632f7f6f
deepakkum21 <43340283+deepakkum21@users.noreply.github.com>
2020-07-31 19:59:29 +0530

    getting all users from the socket object'

--- Chat-App-Socket.io/index.js
+++ Chat-App-Socket.io/index.js
@@ -2,18 +2,43 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+const port = 8888;
+
 app.get('/', function (req, res) {
-    res.sendfile('index.html');
+    //res.sendfile('index.html');
+    res.sendFile(__dirname + '/index.html');
 })
 
-io.on('connection', function(socket) {
-    console.log('A user connected')
+const getUsers = () => {
+    let clients = io.sockets.clients().connected;
+    //console.log('Clients: ', clients);
+    let sockets = Object.values(clients);
+    //console.log('sockets: ',sockets);
+    users = sockets.map(s => s.user);
+    //console.log('users: ', users);
+    return users;
+}
+
+const emitUsers = () => {
+    io.emit('users', getUsers());
+}
+
+io.on('connection', function (socket) {
+    console.log('A user connected');
+
+    socket.on('new_user', user => {
+        console.log('New User: ', user);
+
+        socket.user = user;
+        emitUsers();
+    });
 
     socket.on('disconnect', () => {
         console.log('A user disconnected');
-    })
+        emitUsers();
+    });
 });
 
-http.listen(3000, function() {
-    console.log('listening on *:3000');
+http.listen(port, function () {
+    console.log(`listening on *:${port}`);
 })
