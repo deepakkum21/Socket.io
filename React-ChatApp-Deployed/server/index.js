f554c0400f5e20b6923e6e28da2ef15f558b24d2
deepakkum21 <43340283+deepakkum21@users.noreply.github.com>
2020-08-02 10:40:18 +0530

    server and client running set up

---
+++ React-ChatApp-Deployed/server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const router = require('./router');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(router);
+
+io.on('connection', (socket) => {
+    console.log('Just now a new client connected!!!!');
+
+    socket.on('disconnect', () => {
+        console.log('Sad to see a client disconnected. Hope to see them back soon!!!!!!!!');
+    })
+})
+
+app.listen(PORT , () => {
+    console.log(`Server started at Port: ${PORT}`);
+})
