6b2b3802fb90e39fc3b31c0eeed6b8a93783c0a3
deepakkum21 <43340283+deepakkum21@users.noreply.github.com>
2020-08-02 12:22:03 +0530

    server side user crud done and waiting for sendmessage event done

--- React-ChatApp-Deployed/server/index.js
+++ React-ChatApp-Deployed/server/index.js
@@ -32,6 +32,8 @@ const socketio = require('socket.io');
 const cors = require('cors');
 
 const router = require('./router');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+const { use } = require('./router');
 
 const app = express();
 const server = http.createServer(app);
@@ -43,8 +45,29 @@ app.use(router);
 io.on('connect', (socket) => {
     console.log('Just now a new client connected!!!!');
 
-    socket.on('join', ({ name, room }) => {
-        console.log(name, room);
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        if (error) {
+            return callback(error);
+        }
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+        socket.join(user.room);
+
+        callback();
+    });
+
+
+    // waiting for the sendmessage event sent from UI
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+
+        callback();
     });
 
     socket.on('disconnect', () => {
