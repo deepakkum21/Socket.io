8b175223b623cf991f8a56f1031d13135716e65b
deepakkum21 <43340283+deepakkum21@users.noreply.github.com>
2020-08-02 12:04:58 +0530

    join event complted

--- React-ChatApp-Deployed/client/src/components/Chat/Chat.js
+++ React-ChatApp-Deployed/client/src/components/Chat/Chat.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import queryString from 'query-string';
+import io from "socket.io-client";
 
-const Chat = () => {
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    let socket = '';
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        // socket.emit('join',{ name:name, room: room});
+        // using ES6 syntax for above code
+        socket.emit('join', { name, room }, () => {
+
+        });
+
+
+        // cleanup for useEffect like component unmount
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        };
+
+    },
+        // this is passed to avoid two times connection and this is only run when list values will change
+        [ENDPOINT, location.search])
     return (
         <h1> Chat </h1>
     )
--- React-ChatApp-Deployed/server/index.js
+++ React-ChatApp-Deployed/server/index.js
@@ -1,25 +1,55 @@
+// const express = require('express');
+// const socketio = require('socket.io');
+// const http = require('http');
+
+// const router = require('./router');
+
+// const PORT = process.env.PORT || 5000;
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = socketio(server);
+
+// app.use(router);
+
+// io.on('connect', (socket) => {
+//     console.log('Just now a new client connected!!!!');
+
+//     socket.on('disconnect', () => {
+//         console.log('Sad to see a client disconnected. Hope to see them back soon!!!!!!!!');
+//     })
+// })
+
+// app.listen(PORT , () => {
+//     console.log(`Server started at Port: ${PORT}`);
+// })
+
+
+
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
+const cors = require('cors');
 
 const router = require('./router');
-
-const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+app.use(cors());
 app.use(router);
 
-io.on('connection', (socket) => {
+io.on('connect', (socket) => {
     console.log('Just now a new client connected!!!!');
 
-    socket.on('disconnect', () => {
-        console.log('Sad to see a client disconnected. Hope to see them back soon!!!!!!!!'); 
-    })
-})
+    socket.on('join', ({ name, room }) => {
+        console.log(name, room);
+    });
 
-app.listen(PORT , () => {
-    console.log(`Server started at Port: ${PORT}`);
-})
+    socket.on('disconnect', () => {
+        console.log('Sad to see a client disconnected. Hope to see them back soon!!!!!!!!');
+    });
+});
+
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
