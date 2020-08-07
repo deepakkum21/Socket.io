559b8ce0fdf1ebee76797c0f7fd076f73f415e69
deepakkum21 <43340283+deepakkum21@users.noreply.github.com>
2020-08-07 18:20:03 +0530

    created unique room url

--- Video-Chat(Zoom-clone)/server.js
+++ Video-Chat(Zoom-clone)/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 const server  = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.status(200).send("<h1>Hello World</h1>");
+    //res.status(200).send("<h1>Hello World</h1>");
+    // res.render('room');
+
+    res.redirect(`${uuidv4()}`);
 });
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room });
+})
 
 server.listen(PORT, () => {    
     console.log(`The server is running on port ${PORT}`);
