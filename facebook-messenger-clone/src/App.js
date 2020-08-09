91edd7f1a11035eee6400d3307949a1022efc8ad
deepakkum21 <43340283+deepakkum21@users.noreply.github.com>
2020-08-09 16:00:38 +0530

    prevent deafult refreshing on enter button click

--- facebook-messenger-clone/src/App.js
+++ facebook-messenger-clone/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+  const [messages, setMessages] = useState([]);
+
+  //console.log(messages);
+
+  const sendMessages = (event) => {
+    // to prevent the default behaviour which is refereshing
+    // when ever the eneter button is pressed for a form as for m tend sto submit and refreshes the screen
+    event.preventDefault();
+    
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
       <h1>App</h1>
-      <input/>
-      <button>Eneter</button>
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)} />
+        <button type='submit' onClick={sendMessages}>Send Message</button>
+      </form>
+      {messages.map((message, index) =>
+        <p key={index}>{message}</p>
+      )}
     </div>
   );
 }
