ba322221f02705084311f6f43f0679126746f47d
deepakkum21 <43340283+deepakkum21@users.noreply.github.com>
2020-08-09 15:31:39 +0530

    started facebook-messenger-clone using react

---
+++ facebook-messenger-clone/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App</h1>
+      <input/>
+      <button>Eneter</button>
+    </div>
+  );
+}
+
+export default App;
